d2ccdedca2aa8340aaaa4030d8ccd5a5b12a6e01
Sean Johnson <sean.johnson.ca@gmail.com>
2019-11-18 19:07:55 -0800

    pull and display user top artists

--- src/App.js
+++ src/App.js
@@ -1,28 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import SpotifyLoginButton from './components/SpotifyLoginButton'
+import React, { useState } from "react";
+import axios from "axios";
+import { Button } from "react-bootstrap";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
+const App = () => {
+  const [artists, setArtists] = useState([]);
+
+  const loginSpotify = e => {
+    e.preventDefault();
+    axios
+      .get("http://localhost:3000/login")
+      .then(res => (window.location.href = res.data))
+      .catch(err => console.log(err));
+  };
+
+  const getData = e => {
+    e.preventDefault();
+    axios
+      .get("http://localhost:3000/my-top-artists")
+      .then(res => 
+        setArtists(res.data.items))
+      .catch(err => console.log(err));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <SpotifyLoginButton />
+      <Button variant="outline-primary" onClick={loginSpotify}>
+        Login
+      </Button>
+      <Button variant="outline-primary" onClick={getData}>
+        Get data
+      </Button>
+      <div>
+        <center>
+          <h1>Artist List</h1>
+        </center>
+        {artists.map(artist => (
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">{artist.name}</h5>
+              <img src={artist.images[0].url} alt={artist.name} />
+              <p>{artist.genres[0]}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
