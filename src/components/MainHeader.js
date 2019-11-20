cddc004fb2145f749a2339ad6ace3a5143ef5acd
Sean Johnson <sean.johnson.ca@gmail.com>
2019-11-20 12:38:33 -0800

    display date

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import Moment from "moment";
 
 import Profile from "./Profile";
 
@@ -19,9 +20,14 @@ const MainHeader = () => {
               src={process.env.PUBLIC_URL + "/Spotify_Icon_RGB_Green.png"}
               alt="Spotify Logo"
             />
-            <h1 style={{ width: "auto", margin: "0 0 0 0.5em" }}>
-              My Top Music
-            </h1>
+            <div style={{ width: "auto", margin: "0 0 0 0.5em" }}>
+              <h4 style={{ textAlign: "left", margin: "0" }}>My Top Music</h4>
+              <div>
+                <p style={{ textAlign: "left", margin: "0" }}>
+                  {Moment().format("MMM. Do, YYYY")}
+                </p>
+              </div>
+            </div>
           </span>
         </Col>
         <Col xs={6} style={{ padding: "0", display: "inline-flex" }}>
