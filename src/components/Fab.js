0ced8d9750ea8f80226e84db0bfecb26925dac92
Sean Johnson <sean.johnson.ca@gmail.com>
2019-11-20 13:12:10 -0800

    fab with options

---
+++ src/components/Fab.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import SpeedDial from "@material-ui/lab/SpeedDial";
+import SpeedDialIcon from "@material-ui/lab/SpeedDialIcon";
+import SpeedDialAction from "@material-ui/lab/SpeedDialAction";
+import SaveIcon from "@material-ui/icons/Save";
+import ShareIcon from "@material-ui/icons/Share";
+
+const useStyles = makeStyles(theme => ({
+  speedDial: {
+    position: "fixed",
+    "&.MuiFab-primary": {
+      backgroundColor: "#1DB954"
+    },
+    "&.MuiSpeedDial-directionUp, &.MuiSpeedDial-directionLeft": {
+      bottom: theme.spacing(2),
+      right: theme.spacing(2)
+    },
+    "&.MuiSpeedDial-directionDown, &.MuiSpeedDial-directionRight": {
+      top: theme.spacing(2),
+      left: theme.spacing(2)
+    }
+  }
+}));
+
+const actions = [
+  { icon: <SaveIcon />, name: "Save" },
+  { icon: <ShareIcon />, name: "Share" }
+];
+
+const Fab = () => {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  return (
+    <SpeedDial
+      ariaLabel="SpeedDial example"
+      className={classes.speedDial}
+      icon={<SpeedDialIcon />}
+      onClose={handleClose}
+      onOpen={handleOpen}
+      open={open}
+      direction="up"
+    >
+      {actions.map(action => (
+        <SpeedDialAction
+          key={action.name}
+          icon={action.icon}
+          tooltipTitle={action.name}
+          onClick={handleClose}
+        />
+      ))}
+    </SpeedDial>
+  );
+};
+
+export default Fab;
