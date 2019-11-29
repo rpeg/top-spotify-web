ab33dd66a689e6f3370fe5ade986f161eb4cfca6
Sean Johnson <sean.johnson.ca@gmail.com>
2019-11-28 16:17:19 -0800

    fix grid height

--- src/components/ArtistGrid.js
+++ src/components/ArtistGrid.js
@@ -9,34 +9,40 @@ const ArtistGrid = ({ artists }) => {
     <Row key={chunkedArtists[i].toString()} style={{ paddingTop: `${i > 0 ? '4px' : '0px'}` }}>
       {artistRow.map((artist, j) => (
         <Col key={artistRow[j].toString()} style={{ padding: '0px 2px 0px 2px' }}>
-          <img
-            src={artist.images[0].url}
-            alt={artist.name}
-            style={{
-              objectFit: 'cover',
-              objectPosition: 'center',
-              width: '100%',
-              height: '100%',
-            }}
-          />
-          <div
-            style={{
-              position: 'absolute',
-              zIndex: '10',
-              width: '100%',
-              bottom: '0',
-              marginBottom: '0',
-              background: 'rgba(0,0,0,0.5)',
-            }}
+          <div style={{
+            display: 'flex', paddingBottom: '100%', overflow: 'hidden', position: 'relative',
+          }}
           >
-            <p
+            <img
+              src={artist.images[0] ? artist.images[0].url : ''}
+              alt={artist.name}
               style={{
+                width: '100%',
+                height: '100%',
+                position: 'absolute',
+                objectFit: 'cover',
+                objectPosition: 'center',
+              }}
+            />
+            <div
+              style={{
+                position: 'absolute',
+                zIndex: '10',
+                width: '100%',
+                bottom: '0',
                 marginBottom: '0',
-                color: 'white',
+                background: 'rgba(0,0,0,0.5)',
               }}
             >
-              <b>{artist.name}</b>
-            </p>
+              <p
+                style={{
+                  marginBottom: '0',
+                  color: 'white',
+                }}
+              >
+                <b>{artist.name}</b>
+              </p>
+            </div>
           </div>
         </Col>
       ))}
