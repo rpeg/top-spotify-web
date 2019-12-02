f383daf7a21cbacd6e2281b8436ee2147d56ade2
Sean Johnson <sean.johnson.ca@gmail.com>
2019-12-02 11:28:39 -0800

    groundwork for tests; action test suite

---
+++ src/__tests__/containers/App.test.js
@@ -0,0 +1,19 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import configureStore from 'redux-mock-store';
+
+// import App from '../../containers/App';
+// import * as actions from '../../actions/actions';
+
+// const mockStore = configureStore();
+// const store = mockStore();
+
+// describe('clicking create', () => {
+//   it('displays header', () => {
+//     const div = document.createElement('div');
+//     ReactDOM.render(<App />, div);
+//     ReactDOM.unmountComponentAtNode(div);
+
+//     store.dispatch(actions.setHasClickedCreate());
+//   });
+// });
