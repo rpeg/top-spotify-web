5d079a7e2f44edb10e2dbfbc90ece9cbb5916a5b
Sean Johnson <sean.johnson.ca@gmail.com>
2019-12-05 11:45:03 -0800

    clean blob code

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -70,28 +70,18 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+// https://stackoverflow.com/a/46406124/9661059
 const dataUriToBlob = (dataUri) => {
-// convert base64 to raw binary data held in a string
-  // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
   const byteString = atob(dataUri.split(',')[1]);
-
-  // separate out the mime component
   const mimeString = dataUri.split(',')[0].split(':')[1].split(';')[0];
-
-  // write the bytes of the string to an ArrayBuffer
   const ab = new ArrayBuffer(byteString.length);
-
-  // create a view into the buffer
   const ia = new Uint8Array(ab);
 
-  // set the bytes of the buffer to the correct values
-  for (let i = 0; i < byteString.length; i++) {
+  for (let i = 0; i < byteString.length; i += 1) {
     ia[i] = byteString.charCodeAt(i);
   }
 
-  // write the ArrayBuffer to a blob, and you're done
-  const blob = new Blob([ab], { type: mimeString });
-  return blob;
+  return new Blob([ab], { type: mimeString });
 };
 
 const Controls = () => {
