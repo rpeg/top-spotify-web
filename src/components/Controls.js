63bc336179a9f55047e5d482af63bd1e3b898966
Sean Johnson <sean.johnson.ca@gmail.com>
2019-12-05 11:43:54 -0800

    improved export

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -11,6 +11,7 @@ import Switch from '@material-ui/core/Switch';
 import MenuItem from '@material-ui/core/MenuItem';
 import { makeStyles } from '@material-ui/core/styles';
 import domtoimage from 'dom-to-image';
+import { saveAs } from 'file-saver';
 
 import {
   TimeRanges,
@@ -68,6 +69,30 @@ const useStyles = makeStyles((theme) => ({
     },
   },
 }));
+
+const dataUriToBlob = (dataUri) => {
+// convert base64 to raw binary data held in a string
+  // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+  const byteString = atob(dataUri.split(',')[1]);
+
+  // separate out the mime component
+  const mimeString = dataUri.split(',')[0].split(':')[1].split(';')[0];
+
+  // write the bytes of the string to an ArrayBuffer
+  const ab = new ArrayBuffer(byteString.length);
+
+  // create a view into the buffer
+  const ia = new Uint8Array(ab);
+
+  // set the bytes of the buffer to the correct values
+  for (let i = 0; i < byteString.length; i++) {
+    ia[i] = byteString.charCodeAt(i);
+  }
+
+  // write the ArrayBuffer to a blob, and you're done
+  const blob = new Blob([ab], { type: mimeString });
+  return blob;
+};
 
 const Controls = () => {
   const user = useSelector(selectors.selectUser);
@@ -130,8 +155,9 @@ const Controls = () => {
       },
       width: elm.offsetWidth * scale,
     })
-      .then((dataUrl) => {
-        window.open().document.write(`<div><img src="${dataUrl}" style="position: absolute; width: 100%"/></div>`);
+      .then((dataUri) => {
+        const blob = dataUriToBlob(dataUri);
+        saveAs(blob, 'top-spotify.png');
       })
       .catch((err) => {
         console.error('Could not export PNG', err);
