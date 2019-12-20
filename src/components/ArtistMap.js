651843a91b26d653c2c194a48f3e74d04e6dad4b
Sean Johnson <sean.johnson.ca@gmail.com>
2019-12-19 16:41:41 -0800

    improve map design

--- src/components/ArtistMap.js
+++ src/components/ArtistMap.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { ComposableMap, Geographies, Geography } from 'react-simple-maps';
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  Sphere,
+} from 'react-simple-maps';
 
 import { makeSortedFrequencyArr } from '../lib/frequency';
 
@@ -10,31 +15,61 @@ const ArtistMap = ({ artistCountries }) => {
   const countries = artistCountries.map((a) => a.country);
   const sortedCountries = makeSortedFrequencyArr(countries);
 
-  const max = sortedCountries.map((c) => c.freq).reduce((acc, curr) => Math.max(acc, curr));
-
-  console.log(sortedCountries);
-  console.log(max);
+  const max = sortedCountries
+    .map((c) => c.freq)
+    .reduce((acc, curr) => Math.max(acc, curr));
 
   return (
-    <ComposableMap>
-      <Geographies geography={geoUrl}>
-        {({ geographies }) => geographies.map((geo) => {
-          const match = sortedCountries.find(
-            (c) => c.name.toLowerCase() === geo.properties.NAME_LONG.toLowerCase(),
-          );
+    <div
+      className="svg-container"
+      style={{
+        display: 'inline-block',
+        position: 'relative',
+        width: '100%',
+        paddingBottom: 'calc(100% * 4 / 8)',
+        verticalAlign: 'top',
+        overflow: 'hidden',
+      }}
+    >
+      <ComposableMap
+        className="svg-content"
+        projectionConfig={{
+          rotate: [-10, 0, 0],
+          scale: 147,
+        }}
+        width={800}
+        height={400}
+        style={{ width: '100%', height: 'auto' }}
+        viewBox="0 0 800 400"
+      >
+        <Sphere stroke="#1db954" strokeWidth={2} />
+        <Geographies geography={geoUrl}>
+          {({ geographies }) => geographies.map((geo) => {
+            const match = sortedCountries.find(
+              (c) => c.name.toLowerCase() === geo.properties.NAME_LONG.toLowerCase(),
+            );
 
-          return (
-            <Geography
-              key={geo.rsmKey}
-              geography={geo}
-              fill={match ? '#1db954' : '#EEE'}
-              fillOpacity={match ? `${match.freq / max}` : '0.1'}
-              style={{}}
-            />
-          );
-        })}
-      </Geographies>
-    </ComposableMap>
+            return (
+              <Geography
+                key={geo.rsmKey}
+                geography={geo}
+                style={{
+                  default: {
+                    fill: `${match ? '#1db954' : '#282828'}`,
+                    fillOpacity: `${match ? `${match.freq / max}` : '0.25'}`,
+                  },
+                  hover: {
+                    fill: '#D3D3D3',
+                  },
+                }}
+              />
+            );
+          })}
+        </Geographies>
+      </ComposableMap>
+    </div>
+
+
   );
 };
 
